deb7f4503da113ff454a945e7b89bcc12aad82ad
vincrichard <vincent.richard@elix-inc.com>
2024-10-03 14:28:30 +0900

    [Refactor] Package set up and add __init__ files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-   name='Riken Dataset Fundational Model',
-   version='1.0.0',
-   description='Riken dataset fundational model package',
-   author='Elix-inc',
-   packages=['src']
-)
