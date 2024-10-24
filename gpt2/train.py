0961dc4c1b8c8f0bc8f79b50ef0cd31346c38cf2
David Jimenez <david.jimenez@elix-inc.com>
2024-10-24 16:28:19 +0900

    Train Hot Fix

--- gpt2/train.py
+++ gpt2/train.py
@@ -130,8 +130,8 @@ def get_batch(split):
         data = test_data
 
     ix = torch.randint(len(data), (batch_size,))
-    x = data[ix]
-    y = data[ix]
+    x = data[ix, :-1]
+    y = data[ix, 1:]
     if device_type == 'cuda':
         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
         x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
