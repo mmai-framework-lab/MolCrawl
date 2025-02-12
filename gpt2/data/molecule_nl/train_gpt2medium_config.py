0b34c92bdeaacdc427baddb194a553920ab9bd62
David Jimenez <david.jimenez@elix-inc.com>
2025-02-12 11:41:12 +0900

    Add configuration for training GPT-2 model on molecule dataset

---
+++ gpt2/data/molecule_nl/train_gpt2medium_config.py
@@ -0,0 +1,50 @@
+# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# launch as the following (e.g. in a screen session) and wait ~5 days:
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+
+from molecule_related_nl.utils.tokenizer import MoleculeNatLangTokenizer as Tokenizer
+
+# Medium-Sized GPT2 Model
+
+n_layer = 24
+n_head = 16
+n_embd = 1024
+
+tensorboard = True  # log training metrics to tensorboard
+tensorboard_dir = "runs_train_gpt2_molecule_nl_medium_6e-6wu2000-60000-its"
+out_dir = "out-molecule-nl-gpt2-medium-6e-6wu2000-60000-its"
+
+tokenizer = Tokenizer()
+
+# these make the total batch size be ~0.5M
+# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
+batch_size = 2  # max size in koala
+block_size = 1024
+gradient_accumulation_steps = 5 * 16
+
+# training
+max_iters = 60000
+lr_decay_iters = 60000
+warmup_iters = 2000  # how many steps to warm up for
+learning_rate = 6e-6  # max learning rate
+min_lr = learning_rate/10  # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+
+# eval stuff
+eval_interval = 200
+eval_iters = 200
+log_interval = 200
+
+# weight decay
+weight_decay = 1e-1
+
+# dataset
+dataset = "molecule_nl"
+
+dataset_params = {
+    "dataset_dir": "outputs/training_ready_hf_dataset"
+}
+
+# Special Tokens
+start_instruction = 1
+end_instruction = [518, 29914, 25580, 29962]
+eos_token = 2  # eos
