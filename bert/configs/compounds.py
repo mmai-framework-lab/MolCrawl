7fb18024eed112e23f411c1aa2a39aeab89b59cc
vincrichard <vincent.richard@elix-inc.com>
2025-03-25 18:14:04 +0900

    [Add] bert config compound and genome_sequence

---
+++ bert/configs/compounds.py
@@ -0,0 +1,19 @@
+# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# launch as the following (e.g. in a screen session) and wait ~5 days:
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+
+from compounds.utils.tokenizer import CompoundsTokenizer as Tokenizer
+
+
+tokenizer = Tokenizer("assets/molecules/vocab.txt", 256)
+
+max_steps = 600000
+model_path = "runs_train_bert_compounds"
+max_length = 1024
+dataset_dir = "/nasa/datasets/riken/projects/fundamental_models_202407/compounds/training_ready_hf_dataset"
+learning_rate = 6e-6
+weight_decay = 1e-1
+log_interval = 100
+per_device_train_batch_size = 8
+
+gradient_accumulation_steps = 5 * 16
---
+++ bert/configs/genome_sequence.py
@@ -0,0 +1,41 @@
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from transformers import PreTrainedTokenizerFast, AutoTokenizer
+import sentencepiece as spm
+
+model_path = "runs_train_bert_genome_sequence"
+max_length = 1024
+dataset_dir = "/nasa/datasets/riken/projects/fundamental_models_202407/refseq/training_ready_hf_dataset"
+learning_rate = 6e-6
+weight_decay = 1e-1
+max_steps = 600000
+
+log_interval = 100
+per_device_train_batch_size = 8
+
+gradient_accumulation_steps = 5 * 16
+
+# Tokenizer instantiation
+# -----------------------------------------------------------------------------
+sp = spm.SentencePieceProcessor(model_file="/nasa/datasets/riken/projects/fundamental_models_202407/refseq/spm_tokenizer.model")
+# Get vocabulary size
+vocab_size = sp.get_piece_size()
+
+# Get all tokens in the vocabulary
+vocab = [sp.id_to_piece(i) for i in range(vocab_size)]
+
+
+tmp_tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+
+tmp_tokenizer.add_tokens(vocab)
+
+tmp_tokenizer = PreTrainedTokenizerFast(tokenizer_object=tmp_tokenizer)
+tmp_tokenizer.unk_token = "[UNK]"
+tmp_tokenizer.sep_token = "[SEP]"
+tmp_tokenizer.pad_token = "[PAD]"
+tmp_tokenizer.cls_token = "[CLS]"
+tmp_tokenizer.mask_token = "[MASK]"
+
+tmp_tokenizer.save_pretrained("custom_tokenizer")
+
+tokenizer = AutoTokenizer.from_pretrained("custom_tokenizer")
