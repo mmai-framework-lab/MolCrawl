e1b1081674ca7908fa85c9dffcee3ce791166331
deskull-m <deskull-m@users.noreply.github.com>
2025-09-04 14:38:55 +0900

    [Refactor] Delete comment out in `config/env.sh`

--- src/config/env.sh
+++ src/config/env.sh
@@ -3,15 +3,4 @@
 # 使用方法: source src/config/env.sh
 
 export LEARNING_SOURCE_DIR="learning_source_20250828"
-#export UNIPROT_DATASET_DIR="$LEARNING_SOURCE_DIR/uniprot/training_ready_hf_dataset"
-#export CELLXGENE_DATASET_DIR="$LEARNING_SOURCE_DIR/cellxgene/training_ready_hf_dataset"
-#export COMPOUNDS_DATASET_DIR="$LEARNING_SOURCE_DIR/compounds/training_ready_hf_dataset"
-#export MOLECULE_NL_DATASET_DIR="$LEARNING_SOURCE_DIR/molecule_nl/training_ready_hf_dataset"
-
-echo "環境変数が設定されました:"
 echo "  LEARNING_SOURCE_DIR=$LEARNING_SOURCE_DIR"
-#echo "  UNIPROT_DATASET_DIR=$UNIPROT_DATASET_DIR"
-#echo "  REFSEQ_DATASET_DIR=$REFSEQ_DATASET_DIR"
-#echo "  CELLXGENE_DATASET_DIR=$CELLXGENE_DATASET_DIR"
-#echo "  COMPOUNDS_DATASET_DIR=$COMPOUNDS_DATASET_DIR"
-#echo "  MOLECULE_NL_DATASET_DIR=$MOLECULE_NL_DATASET_DIR"
