992272afa81dd355519828d61a27a59cbbf07f36
deskull-m <deskull-m@users.noreply.github.com>
2025-12-11 17:44:10 +0900

    [Fix] Delete trouble option in `bootstraps/01_compounds_prepare.sh`.

--- bootstraps/01_compounds_prepare.sh
+++ bootstraps/01_compounds_prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "DatabaseDir: $LEARNING_SOURCE_DIR"
 mkdir -p logs
-nohup python scripts/preparation/preparation_script_compounds.py assets/configs/compounds.yaml --force --tokenize-only \
+nohup python scripts/preparation/preparation_script_compounds.py assets/configs/compounds.yaml --force \
 > logs/compounds-preparation-$(date +%Y-%m-%d_%H-%M-%S).log 2>&1 &
